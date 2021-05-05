15b6580b4d635bc56a47a0c7c8c955b572a9f1ff
sudachi0114 <jnevariste@gmail.com>
2021-05-05 12:35:33 +0900

    define User Entity

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/sudachi0114/cleanArchitecture-go-api
+
+go 1.15
--- src/app/main.go
+++ src/app/main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/sudachi0114/cleanArchitecture-go-api/src/app/domain"
+)
 
 func main() {
 	fmt.Println("Hello, cleanArchitecture!")
+
+	user := domain.User{ID: 1, FirstName: "suzuki", LastName: "taro"}
+	fmt.Println(user)
 }

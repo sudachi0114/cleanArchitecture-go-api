08d5b30f4511c691d09a214c3a7864332d19e32d
sudachi0114 <jnevariste@gmail.com>
2021-05-06 19:08:23 +0900

    call and execute Create -> Add -> Store process (本当はダメだけど、動作確認のため..)

--- src/app/main.go
+++ src/app/main.go
@@ -5,14 +5,24 @@ import (
 
 	"github.com/sudachi0114/cleanArchitecture-go-api/src/app/domain"
 	"github.com/sudachi0114/cleanArchitecture-go-api/src/app/infrastructure"
+	"github.com/sudachi0114/cleanArchitecture-go-api/src/app/interfaces/controllers"
 )
 
 func main() {
 	fmt.Println("Hello, cleanArchitecture!")
 
-	user := domain.User{ID: 1, FirstName: "suzuki", LastName: "taro"}
-	fmt.Println(user)
+	taro := domain.User{ID: 1, FirstName: "suzuki", LastName: "taro"}
+	fmt.Println(taro)
 
 	conn := infrastructure.NewSqlHandler()
 	fmt.Println(conn)
+
+	// user_repo := database.UserRepository{}
+	// id, err := user_repo.Store(taro) <- contoller 層を経由して呼び出していないため、実装に結びついていない。なので、実際に走る処理は nil になる。
+	// if err != nil {
+	// 	panic(err.Error)
+	// }
+
+	uc := controllers.NewUserController(infrastructure.NewSqlHandler()) // <- こっちは controller で infrastructure.SqlHandler と database.SqlHandler が結びつくので nil にならない
+	uc.Create(taro)
 }

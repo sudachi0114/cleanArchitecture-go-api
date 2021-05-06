d56c4c0bad0125b7a1f44890d94c19c94f1f64e7
sudachi0114 <jnevariste@gmail.com>
2021-05-06 19:01:43 +0900

    fix infrastructure SqlHandler (strunct) behavior, to comply database layer's interface (dependency inversion: struct <- interface)

--- src/app/infrastructure/sqlhandler.go
+++ src/app/infrastructure/sqlhandler.go
@@ -4,13 +4,14 @@ import (
 	"database/sql"
 
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/sudachi0114/cleanArchitecture-go-api/src/app/interfaces/database"
 )
 
 type SqlHandler struct {
 	Conn *sql.DB
 }
 
-func NewSqlHandler() *SqlHandler {
+func NewSqlHandler() database.SqlHandler {
 	conn, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/sample")
 	if err != nil {
 		panic(err.Error)
@@ -18,4 +19,26 @@ func NewSqlHandler() *SqlHandler {
 	sqlHandler := new(SqlHandler)
 	sqlHandler.Conn = conn
 	return sqlHandler
+}
+
+func (handler *SqlHandler) Execute(statement string, args ...interface{}) (database.Result, error) {
+	res := SqlResult{}
+	result, err := handler.Conn.Exec(statement, args...)
+	if err != nil {
+		return res, err
+	}
+	res.Result = result
+	return res, nil
+}
+
+type SqlResult struct {
+	Result sql.Result
+}
+
+func (r SqlResult) LastInsertId() (int64, error) {
+	return r.Result.LastInsertId()
+}
+
+func (r SqlResult) RowsAffected() (int64, error) {
+	return r.Result.RowsAffected()
 }
